e69eba700126195d161b7f4821b3953d4ba97549
Patil <Patil@SONY-VAIO>
2021-08-01 13:31:59 +0530

    This Is Test1 Module

---
+++ src/Test_Classes/Test1.java
@@ -0,0 +1,9 @@
+package Test_Classes;
+
+public class Test1 
+{
+	 public static void main(String[] args)
+	 {
+		System.out.println("Hello");
+	 }
+}
---
+++ src/Test_Classes/Test2.java
@@ -0,0 +1,15 @@
+package Test_Classes;
+
+public class Test2
+{
+ public static void main(String[] args) 
+ {
+	 System.out.println("Hello.........");
+		
+		System.out.println("How are you....");
+		
+		System.out.println("Good Morning........");
+		
+		System.out.println("Have a nice Day.......");
+ }
+}
